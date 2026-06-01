fac2293e807358a2fe2a45e059650029010d1693
westmntwing-lab <westmnt.wing@gmail.com>
2026-06-02 00:14:55 +0900

    Delete assets directory

--- assets/js/copy.js
+++
@@ -1,7 +0,0 @@
-function copyAddress(id) {
-    const text = document.getElementById(id).innerText;
-
-    navigator.clipboard.writeText(text).then(() => {
-        alert("住所をコピーしました！");
-    });
-}
